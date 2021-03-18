81d88137c6cc9fde0aee7190409c8c8451c8284d
teenmaz <teenmaz@gmail.com>
2021-03-18 17:22:32 +0700

    if you want open http

--- user/src/main.ts
+++ user/src/main.ts
@@ -5,13 +5,17 @@ import { Transport, TcpOptions } from '@nestjs/microservices';
 import { ConfigService } from './services/config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(UserModule, {
+  const app = await NestFactory.create(UserModule);
+  app.enableCors();
+  app.connectMicroservice({
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
-      port: new ConfigService().get('port'),
-    },
-  } as TcpOptions);
-  await app.listenAsync();
+      port: (new ConfigService()).get('port')
+    }
+  });
+
+  await app.startAllMicroservicesAsync();
+  // await app.listen((new ConfigService()).get('httpPort'));
 }
 bootstrap();
